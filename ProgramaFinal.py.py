bfa02f4e919dd5a606e614fbb3368a406f10421b
Glauber Almeida de Brito <glauberalmeidab@gmail.com>
2025-10-22 20:35:05 -0300

    Adição do Programa Final

---
+++ ProgramaFinal.py.py
@@ -0,0 +1,49 @@
+'''
+    Autores: 
+    - Glauber Almeida de Brito
+    - Anna Caroline Nascimento Silva
+    - Maycon Kaio Silva
+    - Luis Henrique N. C. Pozenato
+    Turma: 2ºA DS               Data: 22/10/2025
+
+    Atividade de Dicionário - Glauber X Gustavo dos Santos
+    Faça um programa que:
+    - Tenha tela de login/cadastro
+    - Tenha um menu para escolher o programa desejado:
+        - Programas: Exercício 1, Exercício 2, Exercício 3, Exercício 4, Exercício 5
+    - Tenha uma tela de encerramento
+'''
+'''
+    Exercício 1: Maycon
+    - Faça um programa que Cadastre os nomes dos alunos, suas notas e suas médias. No final mostre:
+    - O nome do aluno, sua média e sua situação, caso o usuário queria ver mais informações exiba as notas dos bimestre.
+'''
+'''
+    Exercício 2: Glauber
+    - Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+    - Guarde esses resultados em um dicionário em Python.
+    - No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+'''
+'''
+    Exercício 3: Anna
+    - Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário.
+    - Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
+    - Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
+'''
+'''
+    Exercício 4: Maycon
+    - Crie um programa que gerencie o aproveitamento de jogadores de futebol.
+    - O programa vai ler o nome dos jogadores e quantas partidas ele jogou.
+    - Depois vair ler a quantidade de gols feitos em cada partida.
+    - No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+'''
+'''
+    Exercício 5: Luis
+    - Crie um programa que leia nome, sexo e idade de várias pessoas.
+    - Guarde os dados de cada pessoa em um dicionário e todos os dicionários em uma lista.
+    - No final, Mostre:
+        A) Quantas pessoas foram cadastradas
+        B) A média de idade
+        C) Uma lista com as mulheres
+        D) Uma lista de pessoas com idade acima da média
+'''
