3ff572eaabf9be70ff087b3464a415b058ba49b6
Pozenatos <luishenrique.n.c.pozenato@gmail.com>
2025-10-23 18:05:27 -0300

    Versão em desenvolvimento do EX05

--- Exercicio5.py
+++ Exercicio5.py
@@ -12,3 +12,42 @@
         C) Uma lista com as mulheres
         D) Uma lista de pessoas com idade acima da média
 '''
+
+from os import system as sys
+from time import sleep
+
+sys("cls")
+
+pessoa = dict()
+pessoas = list()
+loop = True
+
+while loop == True:
+    user_respo = str
+    
+
+    pessoa['nome'] = str(input("Digite o nome do individuo: "))
+    while pessoa['sexo'] != "M" and pessoa['sexo'] != "F":
+
+        pessoa['sexo'] = str(input("Digite o sexo do Individuo: (M/F) ")).upper().strip()
+        if pessoa['sexo'] == "M":
+            print(f"{{pessoas['nome']}} é do sexo masculino. ")
+        elif pessoa['sexo'] == "F":
+            print(f"{{pessoas['nome']}} é do sexo feminino. ")
+        else:
+            print("Resposta inválida, tente novamente")
+            print(pessoa['sexo'])
+        
+    pessoa['idade'] = int(input("Digite a idade do Individuo: "))
+    pessoas.append(pessoa.copy())
+
+    while user_respo != "S" and user_respo != "N":
+        user_respo = str(input('Deseja registrar outra pessoa? (S/N): ')).upper().strip()
+        if user_respo == 'N':
+            loop = False
+        elif user_respo == 'S':
+            loop = True
+        else:
+            print("\nResposta inválida, tente novamente\n")
+print("calculo doidão hahaahahhahahaa\n")  
+print(pessoas) 
