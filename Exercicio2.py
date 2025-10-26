33b0e50017d02d693165c8b9ea3f5571eaa0df05
GlauberAlbz <glauberalmeidab@gmail.com>
2025-10-26 17:12:03 -0300

    Estilização feita
    Observação: Falta perguntar se o usuario deseja jogar novamente

--- Exercicio2.py
+++ Exercicio2.py
@@ -13,6 +13,20 @@ import random as rd
 from os import system as sys
 from time import sleep
 
+black= "\033[0;30m" 
+red= "\033[0;31m"
+green= "\033[0;32m"
+yellow= "\033[0;33m" 
+blue= "\033[0;34m"
+purple= "\033[0;35m"
+cyan= "\033[0;36m"
+orange = "\033[38;5;208m" 
+pink= "\033[38;5;198m"
+brown= "\033[38;5;130m"
+white = "\033[0;37m"
+gray = white + black
+reset= "\033[0m"
+
 jogadores = dict()
 lista_jogadores = list()
 
@@ -21,17 +35,22 @@ for c in range(0,4):
 
     sys('cls')
 
-    print(f'Jogador {c+1}')
+    print(cyan + '╔' + '═' * 73 + '╗')
+    print(f'║' + yellow + f'Jogador {c+1}'.center(73) + cyan + '║')
+    print('╚' + '═' * 73 + '╝' + reset)
     jogadores['nick'] = str(input('Por favor digite o seu nickname antes de iniciar: '))
     jogadores['dado'] = rd.randint(1, 6)
     lista_jogadores.append(jogadores.copy())
     
     sys('cls')
 
-    print(f'Jogador {c+1}')
-    print(f'Seja bem vindo(a) {lista_jogadores[c]['nick']}!')
-    print('Aqui você irá disputar com outros jogadores em uma competição de dados.')
-    print('O jogador que tirar o maior valor vence o jogo!')
+    print(cyan + '╔' + '═' * 73 + '╗')
+    print('║' + yellow + f'Jogador {c+1}'.center(73) + cyan + '║')
+    print('╠' + '═' * 73 + '╣')
+    print('║' + reset + f'Seja bem vindo(a) {lista_jogadores[c]['nick']}!'.center(73) + cyan + '║')
+    print('║' + reset + 'Aqui você irá disputar com outros jogadores em uma competição de dados.'.center(73) + cyan + '║')
+    print('║' + reset + 'O jogador que tirar o maior valor vence o jogo!'.center(73) + cyan + '║')
+    print('╚' + '═' * 73 + '╝' + reset)
     input('Se estiver pronto para começar pressione ENTER...\n')
 
     sys('cls')
@@ -50,16 +69,24 @@ for c in range(0,4):
         sys('cls')
 
     if lista_jogadores[c]['dado'] <= 2:
-        print(f'Que pena {lista_jogadores[c]['nick']}, você tirou {lista_jogadores[c]['dado']}...')
+        print(cyan + '╔' + '═' * 73 + '╗')
+        print('║' + reset + f'Que pena {lista_jogadores[c]['nick']}, você tirou {lista_jogadores[c]['dado']}...'.center(73) + cyan + '║')
+        print('╚' + '═' * 73 + '╝' + reset)
         input('Pressione ENTER para prosseguir...')
     elif lista_jogadores[c]['dado'] > 2 and lista_jogadores[c]['dado'] <= 4:
-        print(f'{lista_jogadores[c]['nick']}, você tirou {lista_jogadores[c]['dado']}.')
+        print(cyan + '╔' + '═' * 73 + '╗')
+        print('║' + reset + f'{lista_jogadores[c]['nick']}, você tirou {lista_jogadores[c]['dado']}.'.center(73) + cyan + '║')
+        print('╚' + '═' * 73 + '╝' + reset)
         input('Pressione ENTER para prosseguir...')
     elif lista_jogadores[c]['dado'] > 4 and lista_jogadores[c]['dado'] < 6:
-        print(f'Parabéns {lista_jogadores[c]['nick']}! Você tirou {lista_jogadores[c]['dado']}!')
+        print(cyan + '╔' + '═' * 73 + '╗')
+        print('║' + reset + f'Parabéns {lista_jogadores[c]['nick']}! Você tirou {lista_jogadores[c]['dado']}!'.center(73) + cyan + '║')
+        print('╚' + '═' * 73 + '╝' + reset)
         input('Pressione ENTER para prosseguir...')
     else:
-        print(f'Que incrível {lista_jogadores[c]['nick']}!!! Você tirou {lista_jogadores[c]['dado']}!!!')
+        print(cyan + '╔' + '═' * 73 + '╗')
+        print('║' + reset + f'Que incrível {lista_jogadores[c]['nick']}!!! Você tirou {lista_jogadores[c]['dado']}!!!'.center(73) + cyan + '║')
+        print('╚' + '═' * 73 + '╝' + reset)
         input('Pressione ENTER para prosseguir...')
 
 sys('cls')
@@ -118,9 +145,10 @@ for i in range(len(lista_jogadores)):
             lista_jogadores[i], lista_jogadores[j] = lista_jogadores[j], lista_jogadores[i]
 
 # Ranking dos Jogadores
-print('🏆 O ranking dos jogadores:')
-print(f'1º lugar: {lista_jogadores[0]['nick']} com {lista_jogadores[0]['dado']}')
-print(f'2º lugar: {lista_jogadores[1]['nick']} com {lista_jogadores[1]['dado']}')
-print(f'3º lugar: {lista_jogadores[2]['nick']} com {lista_jogadores[2]['dado']}')
-print(f'4º lugar: {lista_jogadores[3]['nick']} com {lista_jogadores[3]['dado']}')
-
+print(cyan + '╔' + '═' * 73 + '╗')
+print('╠' + reset + '🏆 O ranking dos jogadores:'.center(72) + cyan + '╣')
+print('║' + yellow + f'1º lugar: {lista_jogadores[0]['nick']} com {lista_jogadores[0]['dado']}'.center(73) + cyan + '║')
+print('║' + gray + f'2º lugar: {lista_jogadores[1]['nick']} com {lista_jogadores[1]['dado']}'.center(73) + cyan + '║')
+print('║' + brown + f'3º lugar: {lista_jogadores[2]['nick']} com {lista_jogadores[2]['dado']}'.center(73) + cyan + '║')
+print('║' + reset + f'4º lugar: {lista_jogadores[3]['nick']} com {lista_jogadores[3]['dado']}'.center(73) + cyan + '║')
+print('╚' + '═' * 73 + '╝' + reset)
