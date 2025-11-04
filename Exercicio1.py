819e647b0801842259ee4f832d5521a3368126f4
GlauberAlbz <glauberalmeidab@gmail.com>
2025-11-04 17:23:46 -0300

    Reformular o fluxo de trabalho e a interface do usuário para gerenciamento de alunos.
    Reformulamos o loop principal do programa para fornecer uma interface baseada em menus para cadastrar, analisar e avaliar alunos. Aprimoramos a validação de entrada, adicionamos instruções mais claras e otimizamos o processo de listagem e seleção de alunos por nível de ensino. Refatoramos os nomes das variáveis ​​para maior consistência e legibilidade, e modularizamos o código para facilitar a manutenção.

--- Exercicio1.py
+++ Exercicio1.py
@@ -42,49 +42,244 @@ bimestre_3_EM = dict()
 bimestre_4_EM = dict()
 boletim_EM = [bimestre_1_EM, bimestre_2_EM, bimestre_3_EM, bimestre_4_EM]
 
-print('Bem vindo!')
-print('Programa de notas.')
+def limpar():
+    sys('cls')
 
+resposta = str()
 loopprincipal = True
+skip = False # Serve para pular o inicio do programa para evitar casos de redundância
+
+# 3 variáveis principais, server para sustentar os loops principais do programa
+cadastrar = False 
+analisar = False
+avaliar = False
 
 while loopprincipal == True:
-    analise = True
-    cadastrar = str(input('Você deseja cadastrar um aluno? S/N'))
+    if skip == False:
+        limpar()
+        print('O que deseja fazer?')
+        print('1 - Cadastrar alunos')
+        print('2 - Analisar alunos')
+        print('3 - Avaliar alunos')
+        resposta = str(input('Digite sua resposta - (1/2/3): ')).strip()
 
-    if cadastrar == 'S':
-        cadastrar = True
-    else:
-        cadastrar = False
+        while resposta not in ['1', '2', '3']:
+            limpar()
+            print('O que deseja fazer?')
+            print('1 - Cadastrar alunos')
+            print('2 - Analisar alunos')
+            print('3 - Avaliar alunos')
+            print('Digito inválido! Tente novamente.')
+            cadastrar = str(input('Digite sua resposta - (1/2/3): ')).strip()
+
+        match resposta:
+            case '1':
+                cadastrar = True
+                analisar = False
+                avaliar = False
+            case '2':
+                cadastrar = False
+                analisar = True
+                avaliar = False
+            case '3':
+                cadastrar = False
+                analisar = False
+                avaliar = True
+
+        skip = False
 
     while cadastrar == True:
+        limpar()
         
-        aluno['Nome'] = str(input('Digite o nome do(da) aluno(a): '))
-        aluno['Grade'] = int(input('Digite a grade do(da) aluno(a): '))
+        aluno['nome'] = str(input('Digite o nome do(da) aluno(a): '))
+        print('╔' + '═' * 73 + '╗')
+        print('║' + 'Grades:'.center(73) + '║')
+        print('╠' + '═' * 73 + '╣')
+        for grades in range(1, 10):
+            print('║' + f'{grades} - {grades}º ano do Ensino Fundamental'.center(73) + '║')
+        print('╠' + '═' * 73 + '╣')
+        for grades in range(1, 4):
+            print('║' + f'{9 + grades} - {grades}ª série do Ensino Médio'.center(73) + '║')
+        print('╚' + '═' * 73 + '╝' + reset)
+        
+        aluno['grade'] = int(input('Digite a grade do(da) aluno(a): ')) ################################################ TEM QUE ARRUMAR, NAO PODE POR LETRA
 
-        if aluno['Grade'] > 0 and aluno['Grade'] < 6: # Adiciona a escolaridade do aluno
-            aluno['Escolaridade'] = 'Ensino Fundamental I'
-        elif aluno['Grade'] > 6 and aluno['Grade'] < 10:
-            aluno['Escolaridade'] = 'Ensino Fundamental II'
-        else:
-            aluno['Escolaridade'] = 'Ensino Médio'
+        while aluno['grade'] not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
+            print('Digito errado')
         
-        if aluno['Escolaridade'] == 'Ensino Fundamental I': # Adiciona o aluno em uma lista com outros da mesma escolaridade
+        # Adicionando a escolaridade do aluno
+        if aluno['grade'] > 0 and aluno['grade'] < 6:
+            aluno['escolaridade'] = 'Ensino Fundamental I'
+        elif aluno['grade'] > 5 and aluno['grade'] < 10:
+            aluno['escolaridade'] = 'Ensino Fundamental II'
+        else:
+            aluno['escolaridade'] = 'Ensino Médio'
+            aluno['grade'] = aluno['grade'] - 9
+        
+        # Adicionando o aluno em uma lista com outros da mesma escolaridade
+        if aluno['escolaridade'] == 'Ensino Fundamental I': 
             alunos_EFI.append(aluno.copy())
-        elif aluno['Escolaridade'] == 'Ensino Fundamental II':
+            aluno = dict()
+        elif aluno['escolaridade'] == 'Ensino Fundamental II':
             alunos_EFII.append(aluno.copy())
+            aluno = dict()
         else:
             alunos_EM.append(aluno.copy())
+            aluno = dict()
         
-        cadastrar = str(input('Você deseja cadastrar outro aluno? S/N'))
-        if cadastrar == 'S':
+        print('Você deseja cadastrar outro aluno?')
+        resposta = str(input('Digite sua resposta - (S/N) ')).strip().upper()
+
+        while resposta not in ['S', 'N']:
+            limpar()
+
+            print('Você deseja cadastrar um aluno?')
+            print('Digito inválido! Tente novamente.')
+            resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+        if resposta == 'S':
             cadastrar = True
+            analisar = False
         else:
             cadastrar = False
+            analisar = False
     
-    while analise == True:
-        analise_escolaridade = str(input('O aluno que você deseja analisar é de qual escolaridade? (EFI/EFII/EM) '))
+    while analisar == True:
+        limpar()
 
-        if analise_escolaridade == 'EFI':
-            for aluno in alunos_EFI:
-                print(f'O aluno {aluno['Nome']} está no {aluno['Grade']} ano do {aluno['Escolaridade']}')
+        print('O aluno que você deseja analisar é de qual escolaridade?')
+        print('1 - Ensino Fundamental I')
+        print('2 - Ensino Fundamental II')
+        print('3 - Ensino Médio')
+
+        resposta = str(input('Digite sua resposta - (1/2/3): ')).strip()
+
+        while resposta not in ['1', '2', '3']:
+            limpar()
+
+            print('O aluno que você deseja analisar é de qual escolaridade?')
+            print('Digito inválido! Tente novamente.')
+            resposta = str(input('Digite sua resposta - (1/2/3): ')).strip()
+
+        if resposta == '1':
+            if len(alunos_EFI) > 0:
+                limpar()
+
+                print('Alunos do Ensino Fundamental I:\n')
+
+                for cont, al in enumerate(alunos_EFI):
+                    print(f'{cont + 1} -> {al['nome']} - {al['grade']}º Ano - {al['escolaridade']}')
+
+                print('Você deseja analisar algum aluno?')
+                resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+                while resposta not in ['S', 'N']:
+                    limpar()
+
+                    print('Alunos do Ensino Fundamental I:\n')
+
+                    for cont, al in enumerate(alunos_EFI):
+                        print(f'{cont + 1} -> {al['nome']} - {al['grade']}º Ano - {al['escolaridade']}')
+
+                    print('Você deseja analisar algum aluno?')
+                    print('Digito inválido! Tente novamente.')
+                    resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+                if resposta == 'S':
+                    limpar()
+                    print('Alunos do Ensino Fundamental I:\n')
+
+                    for cont, al in enumerate(alunos_EFI):
+                        print(f'{cont + 1} -> {al['nome']} - {al['grade']}º Ano - {al['escolaridade']}')
+                    
+                    print(f'\nQual aluno você deseja analizar?')
+                    resposta = str(input(f'Digite sua resposta - (1-{cont + 1}): '))
+
+                    opcoes_validas = [str(i + 1) for i in range(len(alunos_EFI))]
+
+                    while resposta not in opcoes_validas:
+                        print('Alunos do Ensino Fundamental I:\n')
+
+                        for cont, al in enumerate(alunos_EFI):
+                            print(f'{cont + 1} -> {al['nome']} - {al['grade']}º Ano - {al['escolaridade']}')
+
+                        print(f'\nQual aluno você deseja analizar?')
+                        print('Digito Inválido! Tente novamente.')
+                        resposta = str(input(f'Digite sua resposta - (1-{cont + 1}): '))
+
+                    indice = int(resposta) - 1
+                    
+                    limpar()
+
+                    print(f'Aluno: {alunos_EFI[indice]['nome']}')
+                    print(f'Grade: {alunos_EFI[indice]['grade']}')
+                    print(f'Escolaridade: {alunos_EFI[indice]['escolaridade']}')
+
+                    ########################################################################## Aqui vai a parte que mostra os boletins
+                else:
+                    break
+            else:
+                limpar()
+
+                print('Não tem nenhum aluno cadastrado no Ensino Fundamental I!')
+                print('Deseja cadastrar algum aluno?')
+
+                resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+                if resposta == 'S':
+                    cadastrar = True
+                    analisar = False
+                    avaliar = False
+                    skip = True
+                else:
+                    cadastrar = False
+
+        if resposta == '2':
+            if len(alunos_EFI) > 0:
+                for aluno in alunos_EFII:
+                    print(f'{aluno['nome']} - {aluno['grade']}º Ano - {aluno['escolaridade']}')
+                    input()
+            else:
+                limpar()
+
+                print('Não tem nenhum aluno cadastrado no Ensino Fundamental I!')
+                print('Deseja cadastrar algum aluno?')
+
+                resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+                if resposta == 'S':
+                    cadastrar = True
+                    analisar = False
+                    avaliar = False
+                    skip = True
+                else:
+                    cadastrar = False
+
+        if resposta == '3':
+            if len(alunos_EFI) > 0:
+                for aluno in alunos_EM:
+                    print(f'{aluno['nome']} - {aluno['grade']}ª Série - {aluno['escolaridade']}')
+                    input()
+            else:
+                limpar()
+
+                print('Não tem nenhum aluno cadastrado no Ensino Fundamental I!')
+                print('Deseja cadastrar algum aluno?')
+
+                resposta = str(input('Digite sua resposta - (S/N): ')).strip().upper()
+
+                if resposta == 'S':
+                    cadastrar = True
+                    analisar = False
+                    avaliar = False
+                    skip = True
+                else:
+                    cadastrar = False
+                    
+
+
+
+        
+
+
         
